f95401fbb0f2ea97bfb631b6fe873ffc745d3c48
neo <jackson.cloudnative@gmail.com>
2022-06-19 15:25:16 +0800

    init: initialize project
    Signed-off-by: neo <jackson.cloudnative@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# TODO.
+
+# install plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+# install gtags
+
+# install pip install pygments
+
+# Show icons (should install fonts from https://github.com/ryanoasis/nerd-fonts)
